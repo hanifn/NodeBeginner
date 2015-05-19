feb1fc36fbc6e38b153b384bf30364b49891918a
Hanif Norman <hanif@comworth.com.sg>
2015-05-19 17:34:56 +0800

    Finished "Making the request handlers respond"

---
+++ router.js
@@ -0,0 +1,14 @@
+function route(handle, pathname) {
+    console.log("About to route a request for " +
+                pathname);
+
+    if (typeof handle[pathname] === 'function') {
+        return handle[pathname]();
+    } else {
+        console.log("No request handler for " +
+               pathname);
+        return "404 Not found";
+    }
+}
+
+exports.route = route;
