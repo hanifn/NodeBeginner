fb010d219daddf577f78d4dbeb01b96db3837cd2
Hanif Norman <hanif@comworth.com.sg>
2015-06-12 14:16:16 +0800

    Finished "Handling POST requests"

--- router.js
+++ router.js
@@ -1,9 +1,9 @@
-function route(handle, pathname, response) {
+function route(handle, pathname, response, postData) {
     console.log("About to route a request for " +
                 pathname);
 
     if (typeof handle[pathname] === 'function') {
-        return handle[pathname](response);
+        return handle[pathname](response, postData);
     } else {
         console.log("No request handler for " +
                pathname);
